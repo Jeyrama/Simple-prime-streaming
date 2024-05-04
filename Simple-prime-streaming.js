e85bd0ac7522b437620800207e902b7f99b46441
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-05-03 20:14:26 -0700

    Update Simple-prime-streaming.js

--- Simple-prime-streaming.js
+++ Simple-prime-streaming.js
@@ -26,3 +26,19 @@ function solve(a,b){
 }
 
 // or
+
+const solve = (a, b) => primeString(a + b).slice(a, a + b);
+
+const primeString = l => {
+  let s = '';
+  let i = 1;
+  while (s.length < l) 
+    s += isPrime(++i) ? i : '';
+  return s;
+}
+
+const isPrime = n => {
+  for (let i = 2, s = Math.sqrt(n); i <= s; i++)
+    if (n % i === 0) return false;
+  return n > 1;
+}
