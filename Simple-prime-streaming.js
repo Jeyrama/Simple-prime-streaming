110e56e331255b99b61b1e781d9832858227472a
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-05-03 20:08:49 -0700

    Create Simple-prime-streaming.js

---
+++ Simple-prime-streaming.js
@@ -0,0 +1,11 @@
+/*
+Consider a sequence made up of the consecutive prime numbers. 
+This infinite sequence would start with:
+  "2357111317192329313741434753596167717379..."
+
+You will be given two numbers: a and b, 
+and your task will be to return b elements starting from index a in this sequence.
+
+For example:
+  solve(10,5) == `19232` Because these are 5 elements from index 10 in the sequence.
+*/
